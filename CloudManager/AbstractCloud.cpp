1de195d96051e40f129f6ed3405f6f12b0de4b6b
tavplubix <tavplubix@gmail.com>
2015-08-26 16:37:26 +0600

    some changes

---
+++ CloudManager/AbstractCloud.cpp
@@ -0,0 +1,204 @@
+#include "AbstractCloud.h"
+#include "FileClasses.h"
+#include "ConfigFile.h"
+
+
+void AbstractCloud::netLog(QNetworkReply *reply, QIODevice* logDevice) const
+{
+//#if NETLOG
+	//static QFile log("network.log");
+	//log.open(QIODevice::Append);
+	QIODevice* log;
+	if (logDevice) log = logDevice;
+	else if (NETLOG) log = &this->log;
+	else return;
+	log->write("\n======================= RESPONSE =========================\n");
+	int code = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+	log->write("Status code: " + QString::number(code).toLocal8Bit() + "\n");
+	QList<QByteArray> headers = reply->rawHeaderList();
+	for (auto i : headers) log->write(i + ": " + reply->rawHeader(i) + "\n");
+	log->write("BODY:\n");
+	log->write(reply->peek(reply->bytesAvailable() < 1024 ? reply->bytesAvailable():1024));
+	log->write("\n==========================================================\n");
+	//log->flush();
+//#endif
+}
+
+void AbstractCloud::netLog(const QByteArray& type, const QNetworkRequest &request, const QByteArray &body, QIODevice* logDevice) const
+{
+//#if NETLOG
+	//static QFile log("network.log");
+	//log.open(QIODevice::Append);
+	QIODevice* log;
+	if (logDevice) log = logDevice;
+	else if (NETLOG) log = &this->log;
+	else return;
+	log->write("\n======================== REQUEST =========================\n");
+	log->write(type + "  " + request.url().toString().toLocal8Bit() + "\n");
+	QList<QByteArray> headers = request.rawHeaderList();
+	for (auto i : headers) log->write(i + ": " + request.rawHeader(i) + "\n");
+	log->write("BODY:\n");
+	log->write(body);
+	log->write("\n==========================================================\n");
+	//log->flush();
+//#endif
+}
+
+
+void AbstractCloud::waitFor(ReplyID replyid) const
+{
+	const QNetworkReply* reply= replyid;
+	if (reply == nullptr) {
+		qDebug() << "WARNING: reply == nullptr";
+		return;
+	}
+	//WARNING deadlock if reply has been already destroyed
+	/*static*/ QEventLoop loop;
+	connect(reply, &QNetworkReply::destroyed, &loop, &QEventLoop::quit);
+	loop.exec();
+	///*static*/ QTimer timer;
+	//connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+	//timer.start(50);
+	//while (!replyFinished(reply))
+	//{
+	//	loop.exec(QEventLoop::ExcludeUserInputEvents);
+	//}
+}
+
+void AbstractCloud::waitForFinishedSignal(QNetworkReply* reply) const
+{
+	if (reply == nullptr) {
+		qDebug() << "WARNING: reply == nullptr";
+		return;
+	}
+	//WARNING deadlock if reply has been already finished
+	/*static*/ QEventLoop loop;
+	connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+	loop.exec();
+}
+//
+//void QAbstractManager::registerReply(ReplyID reply) const		//FIXME race condition
+//{
+//	replies.insert(reply);
+//}
+//
+//void QAbstractManager::setReplyFinished(ReplyID reply) const	//FIXME race condition
+//{
+//	if (!replies.contains(reply)) {
+//		qDebug() << "ERROR: Race condition detected in setReplyFinished(): Deadlock is possible in waitFor()\n";
+//		//QThread::msleep(42);
+//	}
+//	replies.remove(reply);
+//}
+//
+//bool QAbstractManager::replyFinished(ReplyID reply) const
+//{
+//	if (!replies.contains(reply)) return true;
+//	else return reply->isFinished();
+//}
+
+
+
+AbstractCloud::AbstractCloud(QString qsettingsGroup) 
+	: log("network.log"), qsettingsGroup(qsettingsGroup)
+{
+	log.open(QIODevice::Append);
+	//settings = new QSettings("tavplubix", "CloudManager");		//FIXME разным манагерам нужны разные настройки		//TODO использовать префикс для beginGroup()
+	config = nullptr;
+	m_status = Status::Init;
+	action = ActionWithChanged::SaveNewest;
+}
+
+
+void AbstractCloud::init()
+{
+	if (!authorized()) 
+		waitFor( authorize() );
+	config = new ConfigFile(this);
+	
+	//settings->beginGroup(managerID());
+	//local = settings->value("managedFiles", QVariant::fromValue(ShortNameSet())).value<ShortNameSet>();
+	//settings->endGroup();
+	//сравнить файлы, время последнего изменения
+	m_status = Status::Ready;
+}
+
+AbstractCloud::Status AbstractCloud::status() const
+{
+	return m_status;
+}
+
+void AbstractCloud::setActionWithChanged(ActionWithChanged act)
+{
+	action = act;
+}
+
+QList<LongName> AbstractCloud::managedFiles() const
+{
+	auto tmp = config->filesInTheCloud().toList();
+	QList<LongName> result;
+	for (auto i : tmp) result.push_back(i);
+	return result;
+}
+
+QByteArray AbstractCloud::localMD5FileHash(const LongName& filename)
+{
+	QCryptographicHash hash(QCryptographicHash::Md5);
+	QFile file(filename);
+	file.open(QIODevice::ReadOnly);
+	hash.addData(&file);
+	file.close();
+#if DEBUG
+	return hash.result().toHex();
+#else
+	return hash.result();
+#endif
+}
+
+
+
+void AbstractCloud::syncAll()	
+{
+	auto files = config->filesInTheCloud();
+	for (auto remote : files) {
+		LongName local = remote;
+		QFileInfo localInfo = local;
+		if (!localInfo.exists())
+			downloadFile(remote, new QFile(local));
+		else if (localMD5FileHash(local) != remoteMD5FileHash(remote)) {
+			QDateTime ltime = localInfo.lastModified();
+			QDateTime rtime = lastModified(remote);
+			if (rtime < ltime) uploadFile(remote, new QFile(local));
+			else downloadFile(remote, new QFile(local));
+		}
+		//else file has not been changed
+	}
+
+}
+
+void AbstractCloud::addFile(QFileInfo file)		//FIXME will not work in offline mode	
+{
+	LongName name = file.absoluteFilePath();
+	config->addFile(name);
+	uploadFile(name, new QFile(name));
+}
+
+void AbstractCloud::removeFile(QFileInfo file)	//FIXME will not work in offline mode    
+{
+	LongName name = file.absoluteFilePath();
+	config->removeFile(name);
+}
+
+void AbstractCloud::removeFileData(QFileInfo)	//UNDONE removeFileData()
+{
+	qDebug() << "Not Implemented";
+	throw NotImplemented();
+}
+
+AbstractCloud::~AbstractCloud()
+{
+	//settings->beginGroup(managerID());
+	//settings->setValue("managedFiles", QVariant::fromValue(localFiles));
+	//settings->endGroup();
+	//delete settings;
+}
---
+++ CloudManager/YandexDisk.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <QJsonDocument>
+#include <QJsonObject>
+//#include "WebDAVManager.h"
+#include "AbstractCloud.h"
+#define checkForHTTPErrors(x) _checkForHTTPErrors(x, __FILE__, __LINE__, QT_MESSAGELOG_FUNC); 
+#define HTTPstatus(reply) reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt()
+
+class YandexDisk :
+	//public WebDAVManager
+	public AbstractCloud
+{
+	Q_OBJECT
+public:
+	enum class AuthType : int { Basic, OAuth };
+private:
+	//==================== Hardcoded Const Private Fields ========================
+	const QString appID = "40881b3716704a989c757c30b9922ff2";
+	static const QString clientSecret;
+	const QString callbackURL = "https://oauth.yandex.ru/verification_code";
+	//====================== Private Fields ======================================
+	mutable QNetworkAccessManager disk, *oauth;	
+	AuthType authtype;
+	QByteArray authorizationHeader;
+	time_t validUntil;
+	//====================== Private Methods =====================================
+	bool _checkForHTTPErrors(QNetworkReply *reply, const char* file, const int line, const char* func) const;
+	void createDirIfNecessary(ShortName dirname);
+protected:
+	//======================== Virtual Protected Methods =========================
+	bool authorized() const override final;
+	ReplyID authorize() override final;
+	//QString managerID() const override { return "42"; };
+	qint64 spaceAvailable() const override final;
+	ReplyID downloadFile(const ShortName& name, QSharedPointer<QIODevice> file) override final;
+	ReplyID uploadFile(const ShortName& name, QIODevice* file) override final;
+	ReplyID remove(const ShortName& name) override final;
+	virtual void mkdir(ShortName dir);
+public:
+	//=========================== Public Methods =================================
+	YandexDisk(QString qsettingsGroup = QString());
+	~YandexDisk();
+	YandexDisk::AuthType authType() const { return authtype; }
+	void setAuthType(YandexDisk::AuthType type);
+
+	QDateTime lastModified(const ShortName& name) const override final;
+	QByteArray remoteMD5FileHash(const ShortName& filename) const override final;
+	QString serviceName() override { return "YandexDisk"; };
+	QString userName() override;
+#if DEBUG
+	QByteArray sendDebugRequest(QByteArray requestType, QString url, QByteArray body = QByteArray(),
+		QList<QNetworkReply::RawHeaderPair> additionalHeaders = QList<QNetworkReply::RawHeaderPair>()) override final;
+#endif
+
+	//=========================== Private Qt Slots ===============================
+private slots:
+	//void tokenGot();
+	//void fileDownloaded();
+	//=============================== Qt Signals =================================
+signals :
+
+};
+
